d286e71648b59ccbfae8c78e1b47734062ecfcb4
Pranciskus Macevicius <pranciskus.macevicius@gmail.com>
2025-05-17 02:43:47 +0300

    added custom definitions for generals layers

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -116,7 +116,28 @@ XXX       K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  
 &kp LSHFT K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29  XXX \
                     XXX  XXX  K32  K33  K34       K35  K36  K37  XXX  XXX
 
-#define MIRYOKU_LAYERMAPPING_DOOM MIRYOKU_MAPPING
+#define MIRYOKU_LAYERMAPPING_GENERALS_LETTERS_ALT( \
+     K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
+) \
+&kp ESC   K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  &u_to_U_BASE \
+&kp LCTRL K10  K11  K12  K13  K14                           K15  K16  K17  K18  K19  XXX \
+&kp LSHFT K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29  XXX \
+                    XXX  XXX  K32  K33  K34       K35  K36  K37  XXX  XXX
+
+#define MIRYOKU_LAYERMAPPING_GENERALS_NUMBERS_ALT( \
+     K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                          K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
+) \
+&kp ESC   K00  K01  K02  K03  K04                           K05  K06  K07  K08  K09  &u_to_U_BASE \
+&kp LCTRL K10  K11  K12  K13  K14                           K15  K16  K17  K18  K19  XXX \
+&kp LSHFT K20  K21  K22  K23  K24  XXX  XXX       XXX  XXX  K25  K26  K27  K28  K29  XXX \
+                    XXX  XXX  K32  K33  K34       K35  K36  K37  XXX  XXX
+
 #define MIRYOKU_LAYERMAPPING_DOOM_ALT MIRYOKU_MAPPING
 #define MIRYOKU_LAYERMAPPING_GENERALS MIRYOKU_MAPPING
 #define MIRYOKU_LAYERMAPPING_GENERALS_LETTERS_ALT MIRYOKU_MAPPING
